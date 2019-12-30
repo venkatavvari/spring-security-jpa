e6dc14b39376ffe5c54e8944cc18d999fe8c06bf
venkat_avvari <venkat.avvari1@gmail.com>
2019-12-30 23:47:18 +0530

    initial commit

---
+++ src/main/java/io/javabrains/springsecurityjpa/HomeResource.java
@@ -0,0 +1,25 @@
+package io.javabrains.springsecurityjpa;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping("/")
+    public String hello() {
+        return ("<h1>Hello!!</h1>");
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return ("<h1>Hello User!!</h1>");
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return ("<h1>Hello Admin!!</h1>");
+    }
+
+}
+
